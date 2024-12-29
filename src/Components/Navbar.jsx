90dd9554f3b628f6d68a68c6bea9fe31bed34c55
Aginas pk <aginaspk6@gmail.com>
2024-12-29 14:04:18 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faHeart, faBagShopping, faBars, faX, faUser } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-import { User2, UserCircle2, UserRound } from 'lucide-react'
+import { Heart, LucideShoppingBag, MenuIcon, Search, UserCircle2, X } from 'lucide-react'
 
 function Navbar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
@@ -16,43 +16,67 @@ function Navbar() {
 
 
       {/* sidebar */}
-      <div className={`fixed top-0 left-0  w-100% h-screen bg-[#CF4616] z-50 xl:hidden sm-xl:hidden block transform transition-transform duration-300  ${!isSidebarOpen ? "translate-x-full" : "translate-x-0"}`}>
-        <div className='flex justify-between px-2 mt-10'>
-          <div className='logo text-[34px] leading-[1em] tracking-tighter font-bold text-white px-5 py-[9px]'>HiShoes</div>
-          <div className='flex'>
-            <div className='px-4 py-2 flex items-center'><div className='flex items-center justify-center w-8 h-8 rounded-full bg-white text-lg text-[#CF4616] font-semibold'><FontAwesomeIcon icon={faUser} /></div><h1 className='text-lg text-white px-3'>Log in</h1></div>
-            <div className='px-5 py-2 text-3xl text-white' onClick={openSideNav}><FontAwesomeIcon icon={faX} /></div>
+      <div className={`fixed top-0 left-0  w-[100%] h-screen bg-[#CF4616] z-50 xl:hidden sm-xl:hidden block transform transition-transform duration-300  ${!isSidebarOpen ? "translate-x-full" : "translate-x-0"}`}>
+        <div className='flex justify-between text-white w-full my-5 px-5'>
+          <div className='flex items-center logos'>
+            <h1 className=' text-[50px] line-through xl:text-center xl:px-0 pl-2'>as</h1>
+            <h1 className='pt-[5px]'>awesome sneakers</h1>
+          </div>
+          <div onClick={openSideNav}>
+          <X size={40} className='pt-[6px]'/>
           </div>
 
         </div>
-        <div className='w-full px-8 my-5'>
-          <div className='w-full py-2 text-lg relative text-white'><input type="text" className=' bg-[#CF4616] w-[350px] h-[40px] border-white border-2 rounded-3xl pl-2 pr-10 placeholder:text-white/85 text-base' placeholder='Search Here...' /><FontAwesomeIcon className=' absolute right-5 top-[18px]' icon={faSearch} /></div>
+        <div className='flex flex-col items-center text-2xl mt-5 w-full  text-white '>
+          <h1 className='py-5'>Men</h1>
+          <h1 className='py-5'>Woman</h1>
+          <h1 className='py-5'>Sale</h1>
+          <h1 className='py-5'>Contact</h1>
+          <h1 className='py-5'>About</h1>
+
         </div>
-        <div className='text-white text-xl list-none px-5'>
-          <li className='px-5 py-4'>Men</li>
-          <li className='px-5 py-4'>Woman</li>
-          <li className='px-5 py-4' onClick={() => setIsSidebarOpen(!isSidebarOpen)}>Sales</li>
-          <li className='px-5 py-4'>Stories</li>
+        <div className='flex w-full justify-center mt-5'>
+          <h1 className='text-white text-xl flex gap-2'><UserCircle2 size={25} />Log in</h1>
         </div>
 
       </div>
-      <div className='px-2'>
-      <div className='w-full h-[77px] bg-[#1A2508] flex'>
-        <div className='logos text-white flex'>
-          <h1 className=' text-[42px] line-through '>as</h1>
-          <h1 className='text-[14px]'>awesome sneakers</h1>
+
+
+
+
+
+      <div className='px-2  '>
+        <div className='border-b-2 border-black'>
+          <div className='h-10 bg-[#1A2508] w-full flex justify-center items-center'>
+            <h1 className='text-[13px] leading-[1.6em] font-normal tracking-wider text-white'>Free Shipping for orders over $50</h1>
+          </div>
+          <div className='w-full flex justify-between xl:block '>
+            <h1 className='logos text-[50px] line-through xl:text-center xl:px-0 pl-2'>as</h1>
+            <h1 className='xl:hidden lg:hidden block' onClick={openSideNav}><MenuIcon size={40} /></h1>
+          </div>
+
+
+
+          <div className='flex justify-between py-5 items-center px-2 '>
+            <div className='xl:flex relative'>
+              <h1 className=' absolute left-0 top-[10px]'><Search size={20} /></h1>
+              <input className='pl-7 xl:w-[250px] w-[150px] border-transparent focus:border-b-black focus:border-transparent focus:ring-0 ' placeholder='Search...' type="text" name="" id="" />
+            </div>
+            <div className='xl:flex gap-10 hidden '>
+              <h1>Men</h1>
+              <h1>Woman</h1>
+              <h1>Sale</h1>
+              <h1>Contact</h1>
+              <h1>About</h1>
+
+            </div>
+            <div className='flex gap-8'>
+              <h1 className='xl:flex hidden'><UserCircle2 /><p>Log in</p></h1>
+              <h1><Heart /></h1>
+              <h1><LucideShoppingBag /></h1>
+            </div>
+          </div>
         </div>
-        <div className='text-white flex'>
-          <h1>Men</h1>
-          <h1>Female</h1>
-          <h1>Sale</h1>
-          <h1>Contact us</h1>
-        </div>
-        <div className='text-yellow-400'>
-          <h1><UserCircle2/><p>Log in</p></h1>
-          <h1></h1>
-        </div>
-      </div>
       </div>
 
       {/* content */}
